35f9fb3f0cf6752f6d32a13b067524deb5eb0594
Dana Palmer <roiai4@hotmail.com>
2018-08-05 05:57:23 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood
+  
+  def initialize(name, mood="nervous")
+    @name = name
+    @mood = mood
+  end
+  
+  def self.name
+    @name
+  end
+  
+  def self.mood(new_mood)
+    self.mood = (new_mood)
+  end
+  
 end
